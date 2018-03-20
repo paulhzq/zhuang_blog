a10dccf9bc6c7318931b25e4a0b1d869042eb239
Paul Huang <paul_hzq@hotmail.com>
2018-03-19 22:56:29 -0400

    add posts app urls and views

--- posts/urls.py
+++ posts/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-
+from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include('posts.urls')),
+    url(r'^$',views.post_list),
+    url(r'^create/$',views.post_create),
+    url(r'^detail/$',views.post_detail),
+    url(r'^update/$',views.post_update),
+    url(r'^delete/$',views.post_delete),
+
 ]
